49c07d354c0d79b71d35848fa6da27fa5681416d
lexmin0412 <zhangle_media@hotmail.com>
2022-02-22 17:33:53 +0800

    feat: 支持传入数组，格式化输出

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,76 @@
 import { Logx } from '@tarox/helper-node'
 
 export default (ctx, options) => {
-	const {
-		libName
+
+	let {
+		libs,  // 数组形式
+		libName // 字符串形式 单个库
 	} = options
 
+	// 统一转换成数组方便处理
+	if ( libName ) {
+		libs = [libName]
+	}
+
   ctx.onBuildStart(() => {
-		Logx.start('载入插件 plugin-package-link')
+		Logx.start('载入插件 plugin-package-link', options)
   });
 
 	ctx.modifyBuildTempFileContent(({tempFiles}) => {
+		console.log('');
 		Logx.start('修改通过 link 方式引入的组件引用路径')
 		for (const key in tempFiles) {
 			const { usingComponents } = tempFiles[key].config
 			if (usingComponents && Object.keys(usingComponents).length > 0) {
 				for (const componentName in usingComponents) {
 					let componentPath = usingComponents[componentName];
-					const identiIndex = componentPath.indexOf(libName)
-					if (identiIndex > -1) {
-						Logx.read(`'发现 link 组件路径' ${componentPath}`)
-						usingComponents[componentName] = `${componentPath.slice(0, identiIndex)}/npm/${libName}/${componentPath.slice(identiIndex + libName.length)}`
+
+					let existedLib = ''
+					let libIndex = -1
+					const isLibExisted = libs.some((lib) => {
+						if (componentPath.indexOf(lib) > -1) {
+							existedLib = lib
+							libIndex = componentPath.indexOf(lib)
+						}
+						return componentPath.indexOf(lib) > -1
+					})
+
+					if (isLibExisted) {
+						Logx.read(`link 组件资源 所属组件库: ${existedLib}, 原引入路径: ${componentPath}`)
+						usingComponents[componentName] = `${componentPath.slice(0, libIndex - 4)}/npm/${existedLib}/${componentPath.slice(libIndex + existedLib.length + 1)}`
+						Logx.modify(`link 组件资源引入路径为 ${usingComponents[componentName]}`)
 					}
 				}
 			}
 		}
+		Logx.end('修改通过 link 方式引入的组件引用路径')
+		console.log('');
 	})
 
 	ctx.modifyBuildAssets(({assets}) => {
-		Logx.start('修改通过 link 方式引入的组件输出路径')
+		Logx.start('修改 link 组件资源构建后的输出路径')
 		Object.keys(assets).forEach((srcKey) => {
-			const identifierIndex = srcKey.indexOf(libName)
-			if (identifierIndex === -1) {
+
+			let existedLib = ''
+			let libIndex = -1
+			const isLibExisted = libs.some((lib) => {
+				if (srcKey.indexOf(lib) > -1) {
+					existedLib = lib
+					libIndex = srcKey.indexOf(lib)
+				}
+				return srcKey.indexOf(lib) > -1
+			})
+
+			if (!isLibExisted) {
 				return
 			}
-			Logx.modify(`link 组件输出路径为 npm/${libName}/${srcKey.slice(identifierIndex + libName.length)}`)
-			assets[`npm/${libName}/${srcKey.slice(identifierIndex + libName.length)}`] = assets[srcKey]
+			const newPath = `npm/${existedLib}/${srcKey.slice(libIndex + existedLib.length + 1)}`
+			Logx.read(`link 组件资源 所属组件库: ${existedLib}, 原输出路径: ${srcKey}`)
+			Logx.modify(`link 组件资源输出路径为 ${newPath}`)
+			assets[newPath] = assets[srcKey]
 			Reflect.deleteProperty(assets, srcKey)
 		})
+		Logx.end('修改 link 组件资源构建后的输出路径')
+		console.log('');
 	})
 }
