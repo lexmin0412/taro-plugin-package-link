47e816ec5766221556482b3aaff6e9c96b2621f9
lexmin0412 <zhangle_media@hotmail.com>
2022-02-22 15:58:16 +0800

    chore(release): publish v0.0.5

--- dist/index.js
+++ dist/index.js
@@ -1,44 +1,38 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const helper_node_1 = require("@tarox/helper-node");
 exports.default = (ctx, options) => {
-    const { helper: { chalk } } = ctx;
     const { libName } = options;
     ctx.onBuildStart(() => {
-        console.log(chalk.yellow('插件 plugin-package-link'), '编译开始');
-        console.log('options:', options);
+        helper_node_1.Logx.start('插件 plugin-package-link');
     });
     ctx.modifyBuildTempFileContent(({ tempFiles }) => {
-        console.log('修改link ui lib 引用路径');
+        helper_node_1.Logx.start('修改通过 link 方式引入的组件引用路径');
         for (const key in tempFiles) {
-            if (Object.hasOwnProperty.call(tempFiles, key)) {
-                const usingComponents = tempFiles[key].config['usingComponents'];
-                if (usingComponents) {
-                    for (const componentName in usingComponents) {
-                        if (Object.hasOwnProperty.call(usingComponents, componentName)) {
-                            let componentPath = usingComponents[componentName];
-                            const identiIndex = componentPath.indexOf(libName);
-                            if (identiIndex > -1) {
-                                usingComponents[componentName] = `${componentPath.slice(0, identiIndex)}/npm/${libName}/${componentPath.slice(identiIndex + libName.length)}`;
-                            }
-                        }
+            const usingComponents = tempFiles[key].config;
+            if (usingComponents) {
+                for (const componentName in usingComponents) {
+                    let componentPath = usingComponents[componentName];
+                    const identiIndex = componentPath.indexOf(libName);
+                    if (identiIndex > -1) {
+                        helper_node_1.Logx.read(`'发现 link 组件路径' ${componentPath}`);
+                        usingComponents[componentName] = `${componentPath.slice(0, identiIndex)}/npm/${libName}/${componentPath.slice(identiIndex + libName.length)}`;
                     }
                 }
             }
         }
     });
     ctx.modifyBuildAssets(({ assets }) => {
-        console.log('修改link ui lib输出路径');
+        helper_node_1.Logx.start('修改通过 link 方式引入的组件输出路径');
         Object.keys(assets).forEach((srcKey) => {
             const identifierIndex = srcKey.indexOf(libName);
             if (identifierIndex === -1) {
                 return;
             }
+            helper_node_1.Logx.modify(`link 组件输出路径为 npm/${libName}/${srcKey.slice(identifierIndex + libName.length)}`);
             assets[`npm/${libName}/${srcKey.slice(identifierIndex + libName.length)}`] = assets[srcKey];
             Reflect.deleteProperty(assets, srcKey);
         });
-    });
-    ctx.onBuildFinish(() => {
-        console.log(chalk.blue('插件 '), '编译结束');
     });
 };
 //# sourceMappingURL=index.js.map
